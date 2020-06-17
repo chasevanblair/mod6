8b53937aed0c429db858469e9728b474b7168690
chasevanblair <crvanblair@dmacc.edu>
2020-06-17 02:54:36 -0500

    final

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,2 +1,19 @@
+"""
+
+Program: string_functions.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/17/20
+
+
+The purpose of this program is to show proficiency with
+function parameters
+
+"""
+
+
 def multiply_string(message, n):
-    pass
+    """returns message in a string n times"""
+    return message * n
+
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -1,3 +1,16 @@
+"""
+
+Program: cast_to_integer.py
+
+Author: Your Name
+
+Last date modified: MM/DD/YYYY
+
+
+The purpose of this program is to accept any input,
+convert to a integer type and output the integer.
+
+"""
 from more_functions.string_functions import multiply_string
 import unittest
 
@@ -7,4 +20,10 @@ class test_strings(unittest.TestCase):
     # instructions. If it is supposed to fail and throw an error I can't figure out how to do that(this is when func
     # is just a pass). I'll take missed points if its wrong but I'd appreciate an explanation on how to do it
     def test_multiply_string(self):
+        """tests multiply_string function"""
         multiply_string("ayayay", 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
